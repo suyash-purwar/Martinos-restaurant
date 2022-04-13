ecfe39e6053bf7371ca2ea4467e1e471a8a8f2d8
suyash-purwar <purwarsuyash06@gmail.com>
2022-04-13 06:26:34 +0530

    Made imports in main.scss, changed gulpfile, and removed sass package.

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+const { src, dest, series, watch } = require("gulp")
+const sass = require("gulp-sass");
+const browsersync = require("browser-sync");
+
+const sendHtml = () => {
+    return src("src/**/*.html")
+        .pipe(dest("dist/"));
+}
+
+const sendAssets = () => {
+    return src("src/assets/**/*")
+        .pipe(dest("dist/assets/"));
+}
+
+const compileSass = () => {
+    return src("src/sass/*.scss")
+        .pipe(sass())
+        .pipe(dest("dist/css/"));
+}
+
+const browswesyncServe = cb => {
+    browsersync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+
+    cb();
+}
+
+const browsersyncReload = cb => {
+    browsersync.reload();
+    cb();
+}
+
+const watchTask = () => {
+    watch('src/**/*.html', series(sendHtml, browsersyncReload));
+    watch('src/sass/**/*.scss', series(compileSass, browsersyncReload));
+    watch('src/assets/**/*.{svg, png}', series(sendAssets, browsersyncReload));
+}
+
+exports.default = series(
+    sendHtml,
+    sendAssets,
+    compileSass,
+    browswesyncServe,
+    watchTask
+);
